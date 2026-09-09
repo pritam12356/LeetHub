29475860f4d88867f1e8a13a9b408074efd78eec
pritam12356 <124249925+pritam12356@users.noreply.github.com>
2026-09-09 22:23:27 +0530

    Time: 1 ms (96.76%), Space: 47 MB (96.52%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,26 +1,47 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int ind=0;
+    //     int ind=0;
         
 
-       while(ind<nums.length){
+    //    while(ind<nums.length){
 
-        int leftSum=0, rightSum=0;
+    //     int leftSum=0, rightSum=0;
 
-         for(int i=0;i<ind;i++){
-            leftSum+=nums[i];
+    //      for(int i=0;i<ind;i++){
+    //         leftSum+=nums[i];
+    //     }
+
+    //     for(int i=ind+1;i<nums.length;i++){
+    //         rightSum+=nums[i];
+    //     }
+        
+    //     if(leftSum == rightSum) return ind;
+        
+    //     ind++;
+    //    }
+
+    //    return -1;
+
+
+        int n= nums.length;
+        int pivot=0;
+        int prefixSum[]= new int [n];
+
+        prefixSum[0]= nums[0];
+
+        for(int i=1;i<n;i++){
+            prefixSum[i]= prefixSum[i-1] + nums[i];
         }
 
-        for(int i=ind+1;i<nums.length;i++){
-            rightSum+=nums[i];
+        while(pivot<n){
+            int temp= (pivot==0)?0:prefixSum[pivot-1];
+            if(temp == (prefixSum[n-1] - prefixSum[pivot])){
+                return pivot;
+            }
+            pivot++;
         }
-        
-        if(leftSum == rightSum) return ind;
-        
-        ind++;
-       }
 
-       return -1;
+        return -1;
     }
 }
 
